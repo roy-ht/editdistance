76c87d06efaac19350d870cd1c95229ed0a66c29
Hiroyuki Tanaka <aflc0x@gmail.com>
2019-03-10 15:23:40 +0900

    Add alias method named "distance"

--- editdistance/__init__.py
+++ editdistance/__init__.py
@@ -1,2 +1,9 @@
 from .bycython import eval
-__all__ = ('eval',)
+
+
+def distance(*args, **kwargs):
+    """"An alias to eval"""
+    return eval(*args, **kwargs)
+
+
+__all__ = ('eval', 'distance')
